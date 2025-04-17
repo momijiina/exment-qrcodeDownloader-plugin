5a0e0a3e52773c299c62617250ad408040b85c59
momijiina <93751198+momijiina@users.noreply.github.com>
2025-04-17 09:27:27 +0900

    Update Plugin.php

--- Plugin.php
+++ Plugin.php
@@ -11,16 +11,30 @@ class Plugin extends PluginButtonBase
      */
     public function execute()
     {
-        // 現在のURLを取得（必要に応じてカスタムURLでもOK）作成中です
-        //$url = request()->input('currentUrl', 'https://example.com');
+        // プロキシ環境を考慮したurlの取得
+        $protocol = 'http://';
+        if (!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] !== 'off') {
+            $protocol = 'https://';
+        } elseif (!empty($_SERVER['HTTP_X_FORWARDED_PROTO']) && $_SERVER['HTTP_X_FORWARDED_PROTO'] === 'https') {
+            $protocol = 'https://';
+        }
 
-        // 列(title)の値を取得
-        $getValue = $this->custom_value->getValue('title');
- 
-        $png = QrCode::format('png')->encoding('UTF-8')->size(300)->generate("$getValue");
+        //ID取得
+        $id = $this->custom_value->id;
+        //テーブル名取得
+        $table_name = $this->custom_table->table_name;
+        //Exmentインストール先にサブディレクトリを含む場合は追加してください。例 /exment/admin/data/
+        $param1 = "/admin/data/";
+
+
+        $url = $protocol . $_SERVER['HTTP_HOST'] . $param1 . $table_name . "/" .$id;
 
         //URLの場合
-        //$png = QrCode::format('png')->encoding('UTF-8')->size(300)->generate("$url");
+        $png = QrCode::format('png')->encoding('UTF-8')->size(300)->generate("$url");
+
+        // 列(title)の値を取得 列などの値を取得する場合
+        //$getValue = $this->custom_value->getValue('title');
+        //$png = QrCode::format('png')->encoding('UTF-8')->size(300)->generate("$getValue");
 
         return [
             'fileBase64' => base64_encode($png),
